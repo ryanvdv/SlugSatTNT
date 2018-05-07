2787fe53880771fa69dc833df955649eb26db2f6
Andrew Daixum Matthews <admatthe@crm.ucsc.edu>
2018-05-07 16:21:13 -0700

    coding

---
+++ TransceiverSourceCode.X/main.c
@@ -0,0 +1,24 @@
+/*
+ * File:   main.c
+ * Author: admatthe
+ *
+ * Created on May 7, 2018, 1:33 PM
+ */
+
+
+#include "xc.h"
+#include "plib.h"
+
+// Configuration settings
+#pragma config POSCMOD = HS, FNOSC = PRIPLL
+#pragma config PLLMUL = MUL_18, PLLIDIV = DIV_2
+#pragma config FWDTEN = OFF
+
+int main(void) {
+    
+
+
+    
+    
+    while (1);
+}
