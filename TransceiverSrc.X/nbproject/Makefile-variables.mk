30fa28de4c4b05f10cb1c2b0b613cf7bb264a925
Andrew Daixum Matthews <admatthe@crm.ucsc.edu>
2018-05-31 13:26:15 -0700

    update

---
+++ TransceiverSrc.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=TransceiverSrc.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/TransceiverSrc.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=transceiversrc.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/transceiversrc.x.tar
