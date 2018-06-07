7e51d88ec08101f1dd3295ff25256c77d6590a62
admatthe@ucsc.edu <admatthe@ucsc.edu>
2018-06-06 23:02:09 -0700

    spi library progress

--- TransceiverSrc.X/build/default/production/hal_spi.o.d
+++ TransceiverSrc.X/build/default/production/hal_spi.o.d
@@ -1,2 +1,3 @@
 build/default/production/hal_spi.o:  \
-hal_spi.c 
+hal_spi.c  \
+hal_spi.h 
--- TransceiverSrc.X/hal_spi.h
+++ TransceiverSrc.X/hal_spi.h
@@ -8,14 +8,30 @@
 #ifndef HAL_SPI_H
 #define	HAL_SPI_H
 
-#define SPI_CS_N_PIN LATDbits.LATD4
+/*
+ * CONSTANTS
+ */
+#define SPI_CS_N_PIN_DIR  TRISDbits.TRISD4
+#define SPI_CS_N_PIN      LATDbits.LATD4
 
-#define SPI_BEGIN()       SPI_CS_N_PIN = 0
-#define SPI_TX(x)         SPI2BUF = x
-#define SPI_WAIT_READY()  while (!SPI2STATbits.SPIRBF);
-#define SPI_RX()          SPI2BUF
-#define SPI_END()         SPI_CS_N_PIN = 1
+#define RADIO_BURST_ACCESS   0x40
+#define RADIO_SINGLE_ACCESS  0x00
+#define RADIO_READ_ACCESS    0x80
+#define RADIO_WRITE_ACCESS   0x00
 
+/*
+ * MACROS
+ */
+#define SPI_BEGIN()       SPI_CS_N_PIN = 0                // Pull CSn low to start communication
+#define SPI_TX(x)         SPI2BUF = x                     // Load x into SPI buffer  
+#define SPI_WAIT_READY()  while (!SPI2STATbits.SPIRBF);   // Wait for receive buffer to fill
+#define SPI_RX()          SPI2BUF                         // SPI buffer to read
+#define SPI_END()         SPI_CS_N_PIN = 1                // Pull CSn high to end communication
+
+/*
+ * PROTOTYPES
+ */
+void trxRfSpiInterfaceInit(int prescalerValue);
 
 #endif	/* HAL_SPI_H */
 
